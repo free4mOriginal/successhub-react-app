0a7ecf374e083d0cc8bcacbe19cc9fe25584f58a
free4mOriginal <free4m@gmail.com>
2019-08-09 11:35:52 -0500

    Add components - Header, Navbar; style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import NavBar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// MUI stuff:
+import {
+  withStyles,
+  MuiThemeProvider,
+  createMuiTheme
+} from "@material-ui/core";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#fee0b8",
+      main: "#fe9814",
+      dark: "#7f4c0a",
+      contrastText: "#fff"
+    }
+  },
+  typography: {
+    userNextVariants: true,
+    fontFamily: {
+      primary: "Roboto",
+    }
+  }
+});
+
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider theme={theme}>
+        <NavBar />
+        <header>
+          <h1>The Beer Bank</h1>
+          <h5>Find your favorite beer here</h5>
+        </header>
+        <main />
+      </MuiThemeProvider>
+    );
+  }
 }
 
 export default App;
