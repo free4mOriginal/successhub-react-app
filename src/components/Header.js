1025e43313f1baa92a164e39abf6b72e485b2d50
free4mOriginal <free4m@gmail.com>
2019-08-10 14:09:00 -0500

    add placeholder for input

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
       <div className="header">
         <h1>The Beer Bank</h1>
         <h5>Find your favorite beer here</h5>
-        <input type="textfield"></input>
+        <input type="textfield" placeholder="Search for beer name"></input>
       </div>
     </div>
   );
