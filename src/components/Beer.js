9faf50ffa91418db79596ce54ff2fefd24d08917
free4mOriginal <free4m@gmail.com>
2019-08-10 12:38:46 -0500

    fix Data export

--- src/components/Beer.js
+++ src/components/Beer.js
@@ -1,13 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Data from "../util/testJSON";
+
+console.log(Data);
 
 class Beer extends Component {
-    render() {
-        return (
-            <div className="beerCard">
-                
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="beerCard">
+        <img src={Data[0].image_url} alt="Beer" />
+        <div className="container">
+          <h4>
+            <b>John Doe</b>
+          </h4>
+          <p>Architect & Engineer</p>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Beer
+export default Beer;
