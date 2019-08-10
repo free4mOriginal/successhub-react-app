fc54120b14fba695b036e88189e44c1f1c4f2072
free4mOriginal <free4m@gmail.com>
2019-08-10 14:06:57 -0500

    Justify content in flex container

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,17 +7,19 @@ console.log(Data);
 export default class Main extends Component {
   state = {
     beers: []
-  }
+  };
 
   render() {
     return (
       <div className="flex-container">
-        <Beer beerData={Data[0]} />
-        <Beer beerData={Data[1]} />
-        <Beer beerData={Data[2]} />
-        <Beer beerData={Data[3]} />
-        <Beer beerData={Data[4]} />
-        <Beer beerData={Data[5]} />
+        <div className='flex-container inner'>
+          <Beer beerData={Data[0]} />
+          <Beer beerData={Data[1]} />
+          <Beer beerData={Data[2]} />
+          <Beer beerData={Data[3]} />
+          <Beer beerData={Data[4]} />
+          <Beer beerData={Data[5]} />
+        </div>
       </div>
     );
   }
