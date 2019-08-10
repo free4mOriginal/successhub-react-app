88a1713421e34a1576e913dd998ff404fb8873e1
free4mOriginal <free4m@gmail.com>
2019-08-10 17:15:08 -0500

    refactor: move API call to App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,57 @@ import "./App.css";
 import NavBar from "./components/Navbar";
 import Main from "./components/Main";
 import Header from "./components/Header";
+import axios from "axios";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      allBeers: [],
+      favoredBeers: []
+    };
+    this.handleClick = this.handleClick.bind(this);
+    this.searchBeerApi = this.searchBeerApi.bind(this);
+  }
+
+  handleClick() {
+    console.log(this.state.favoredBeers);
+  }
+
+  searchBeerApi() {
+    axios
+      .get("https://api.punkapi.com/v2/beers")
+      .then(res => {
+        console.log("res", res);
+        return res.data.map(beerItem => {
+          return {
+            image_url: beerItem.image_url,
+            title: beerItem.name,
+            tagline: beerItem.tagline,
+            ibu: beerItem.ibu,
+            abv: beerItem.abv,
+            ebc: beerItem.ebc,
+            description: beerItem.description,
+            bestServed: beerItem.food_pairing
+          };
+        });
+      })
+      .then(beerItems => {
+        this.setState({ allBeers: beerItems });
+      })
+      .catch(err => console.log(err));
+  }
+
+  componentDidMount() {
+    this.setState({ allBeers: this.searchBeerApi() });
+  }
+
   render() {
     return (
       <div>
         <NavBar />
-        <Header />
-        <Main />
+        <Header handleClick={this.handleClick} />
+        <Main allBeers={this.state.allBeers} />
       </div>
     );
   }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,52 +1,13 @@
 import React, { Component } from "react";
 import Beer from "./Beer";
-import Data from "../util/testJSON";
-import axios from "axios";
 
 export default class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      allBeers: [],
-      favoredBeers: []
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    console.log(this.state.favoredBeers);
-  }
-
-  componentDidMount() {
-    axios
-      .get("https://api.punkapi.com/v2/beers")
-      .then(res => {
-        console.log("res", res);
-        return res.data.map(beerItem => {
-          return {
-            image_url: beerItem.image_url,
-            title: beerItem.name,
-            tagline: beerItem.tagline,
-            ibu: beerItem.ibu,
-            abv: beerItem.abv,
-            ebc: beerItem.ebc,
-            description: beerItem.description,
-            bestServed: beerItem.food_pairing
-          };
-        });
-      })
-      .then(beerItems => {
-        this.setState({ allBeers: beerItems });
-      })
-      .catch(err => console.log(err));
-  }
-
   render() {
     return (
       <div className="flex-container">
         <div className="flex-container inner">
-          {this.state.allBeers ? (
-            this.state.allBeers.map(item => {
+          {this.props.allBeers ? (
+            this.props.allBeers.map(item => {
               return (
                 <Beer
                   key={item.title}
