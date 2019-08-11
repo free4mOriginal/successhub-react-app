e8761faf21c583c2fda307cff60b92a0227e2275
free4mOriginal <free4m@gmail.com>
2019-08-10 20:07:23 -0500

    add search clicking functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default function Header() {
-  return (
-    <div className="header-container">
-      <div className="header">
-        <h1>The Beer Bank</h1>
-        <h5>Find your favorite beer here</h5>
-        <input type="textfield" placeholder="Search for beer name"></input>
-        <i class="fas fa-search"></i>
+export class Header extends Component {
+  render() {
+    return (
+      <div className="header-container">
+        <div className="header">
+          <h1>The Beer Bank</h1>
+          <h5>Find your favorite beer here</h5>
+          <input type="textfield" placeholder="Search for beer name" />
+          <i
+            className="fas fa-search"
+            onClick={() => this.props.handleClick}
+          />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default Header;
