61043588ea8511ea2f682627018f0e0043df06b1
free4mOriginal <free4m@gmail.com>
2019-08-11 08:25:40 -0500

    refactor

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,16 @@
 import React, { Component } from "react";
 
-export class Navbar extends Component {
-  constructor(props) {
-    super(props);
-    this.handleSearch = this.handleSearch.bind(this);
-  }
-
-  handleSearch() {
-    this.props.searchBeerApi('');
-  }
-
+export default class Navbar extends Component {
   render() {
     return (
       <div className="nav">
-        <span onClick={this.handleSearch}>
-          <i className="fas fa-home" /> HOME
+        <span onClick={() => this.props.searchBeerApi('')}>
+          <i className="fas fa-home"/> HOME
         </span>
         <span>
-          <i className="fas fa-star" />
-          FAVORITE
+          <i className="fas fa-star"/> FAVORITE
         </span>
       </div>
     );
   }
 }
-
-export default Navbar;
